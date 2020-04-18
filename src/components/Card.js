0be6a79555bef2653612250ec24440ddeef4ca98
PiIsFour <PiIsFour@users.noreply.github.com>
2020-04-18 22:05:32 +0200

    card design draft

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Card.css'
 
-const Card = () => {
-	return <div className="card"></div>
+const Slot = () => {
+	return <div className="slot"></div>
+}
+
+const Card = ({title, description, slots}) => {
+	return <div className="card">
+		<h2>{title}</h2>
+		<p>{description}</p>
+		<div className="slots">
+			{slots.map((s, i) => <Slot {...s} key={i} />)}
+		</div>
+	</div>
 }
 
 export default Card
