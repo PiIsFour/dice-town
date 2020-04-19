3af203ad7a270fe7d120cdd74e1e8e9bb8e682d8
PiIsFour <PiIsFour@users.noreply.github.com>
2020-04-19 15:55:21 +0200

    publish test build

---
+++ precache-manifest.e1b2b1e51617fa98fb02cdbed997333e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38b904833a7e41ca591cc5322715fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6224850e249050de4f",
+    "url": "/static/css/main.663f91d7.chunk.css"
+  },
+  {
+    "revision": "1ceec7aac2a81d1ef0b2",
+    "url": "/static/js/2.bf9eb9d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf9eb9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6224850e249050de4f",
+    "url": "/static/js/main.ba6664f7.chunk.js"
+  },
+  {
+    "revision": "4c7c777f4c55baeab402",
+    "url": "/static/js/runtime-main.f11a1fec.js"
+  }
+]);
