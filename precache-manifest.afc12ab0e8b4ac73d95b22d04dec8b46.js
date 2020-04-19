a11610690854fe3bdf65c8ab0e0952bdc148e669
PiIsFour <PiIsFour@users.noreply.github.com>
2020-04-19 15:59:12 +0200

    fix basepath

---
+++ precache-manifest.afc12ab0e8b4ac73d95b22d04dec8b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4f9cce5ec68825ddd371adc4f978e1",
+    "url": "/dice-town/index.html"
+  },
+  {
+    "revision": "c872512ba5b6c2c92b1c",
+    "url": "/dice-town/static/css/main.663f91d7.chunk.css"
+  },
+  {
+    "revision": "1ceec7aac2a81d1ef0b2",
+    "url": "/dice-town/static/js/2.bf9eb9d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dice-town/static/js/2.bf9eb9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c872512ba5b6c2c92b1c",
+    "url": "/dice-town/static/js/main.53e9253e.chunk.js"
+  },
+  {
+    "revision": "5b2544ebda93a083fd28",
+    "url": "/dice-town/static/js/runtime-main.713be76a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1b2b1e51617fa98fb02cdbed997333e.js"
+  "/dice-town/precache-manifest.afc12ab0e8b4ac73d95b22d04dec8b46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice-town/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
